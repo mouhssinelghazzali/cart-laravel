0c90d7b6a08fd88de58977043388819e5a13dfc2
mouhssine el ghazzali <mouhssine.elghazzali@gmail.com>
2019-11-07 11:16:17 +0100

    first commit

---
+++ resources/views/store/store.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="section">
+        <div class="jumbotron">
+            <h1 class="display-4">Hello, world!</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <hr class="my-4">
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <p class="lead">
+            <a class="btn btn-primary btn-lg" href="{{route('products.index')}}" role="button">Learn more</a>
+            </p>
+          </div>
+    </div>
+    <section>
+        <div class="row">
+        @foreach($latestproducts  as $product)
+        <div class="col-md-4">
+                <div class="card" >
+                <img class="card-img-top" src="{{$product->image}}" alt="Card image cap">
+                        <div class="card-body">
+                          <h5 class="card-title">{{$product->title}}</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <p><strong> $ {{ $product->price }}</strong></p>
+                          <a href="#" class="btn btn-primary">Buy</a>
+                        </div>
+                </div>
+        </div>
+
+        @endforeach
+        </div>
+
+    </section>
+</div>
+
+@endsection
